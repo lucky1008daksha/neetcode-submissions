3ffd71a1eb5bccd1d8abb546c5190bc60d6af959
laxmiNarasimha <159432857+lucky1008daksha@users.noreply.github.com>
2026-07-05 23:38:13 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //Brute- Force method using the visited array of boolean ... 
+        List<List<String>> res = new ArrayList<>();
+        int n = strs.length;
+        boolean[] visited = new boolean[strs.length];
+        for(int i = 0; i < n; i++){
+            if(visited[i]){
+                continue;
+            }
+            List<String> list = new ArrayList<>();
+            visited[i] = true;
+            list.add(strs[i]);
+            for(int j = i + 1; j < n; j++){
+                if(!visited[j] && isAnagram(strs[i], strs[j])){
+                    list.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int n = s.length();
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        for (int i : freq) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
